b21dcbb24ac3f6a8747b55effc0b6d50477957c1
Homer <Home@fdasdar>
2025-03-19 13:57:13 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='grid place-items-center h-screen w-screen text-2xl'>
+        Hello world!
+      </div>
+    </>
+  )
+}
+
+export default App
